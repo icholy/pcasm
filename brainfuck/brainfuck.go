24535c0d1bc3b71a96d90182134763f29660302d
Ilia Choly <ilia.choly@gmail.com>
2018-11-04 22:45:12 -0500

    start writing the compiler

--- brainfuck/brainfuck.go
+++ brainfuck/brainfuck.go
@@ -64,7 +64,17 @@ func (l Loop) String() string {
 	return fmt.Sprintf("[%s]", strings.Join(nodes, ", "))
 }
 
-func Parse(pos int, tokens []Token) ([]Node, int, error) {
+type Program []Node
+
+func (p Program) String() string {
+	nodes := make([]string, len(p))
+	for i, n := range p {
+		nodes[i] = n.String()
+	}
+	return strings.Join(nodes, ", ")
+}
+
+func ParseNodes(pos int, tokens []Token) ([]Node, int, error) {
 	var nodes []Node
 	for pos = pos; pos < len(tokens); pos++ {
 		tok := tokens[pos]
@@ -76,7 +86,7 @@ func Parse(pos int, tokens []Token) ([]Node, int, error) {
 		case CLOSE:
 			return nodes, pos, nil
 		case OPEN:
-			lnodes, lpos, err := Parse(pos+1, tokens)
+			lnodes, lpos, err := ParseNodes(pos+1, tokens)
 			if err != nil {
 				return nil, lpos, err
 			}
@@ -92,14 +102,78 @@ func Parse(pos int, tokens []Token) ([]Node, int, error) {
 	return nil, 0, fmt.Errorf("missing %s", EOF)
 }
 
+func Parse(tokens []Token) (Program, error) {
+	nodes, _, err := ParseNodes(0, tokens)
+	return Program(nodes), err
+}
+
+type Labels struct {
+	count int
+}
+
+func (l *Labels) Next() string {
+	l.count++
+	return fmt.Sprintf("label%d", l.count)
+}
+
+var (
+	DataLabel        = "Data"
+	DataPointerLabel = "DataPointer"
+)
+
+func CompileOp(op Op, labels *Labels) (string, error) {
+	switch Token(op) {
+	case GT:
+		return fmt.Sprintf("inc [%s]", DataPointerLabel), nil
+	case LT:
+		return fmt.Sprintf("dec [%s]", DataPointerLabel), nil
+	default:
+		return "", fmt.Errorf("unsuported op: %s", op)
+	}
+}
+
+func CompileNode(node Node, labels *Labels) ([]string, error) {
+	var instructions []string
+	switch node := node.(type) {
+	case Op:
+		ins, err := CompileOp(node, labels)
+		if err != nil {
+			return nil, err
+		}
+		instructions = append(instructions, ins)
+	default:
+		return nil, fmt.Errorf("unsuported node: %s", node)
+	}
+	return instructions, nil
+}
+
+func Compile(p Program) ([]string, error) {
+	var (
+		labels       Labels
+		instructions []string
+	)
+	for _, n := range p {
+		ins, err := CompileNode(n, &labels)
+		if err != nil {
+			return nil, err
+		}
+		instructions = append(instructions, ins...)
+	}
+	return instructions, nil
+}
+
 func main() {
-	input := "[->+<]"
+	input := "><"
 	tokens := Tokenize(input)
-	nodes, _, err := Parse(0, tokens)
+	program, err := Parse(tokens)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, n := range nodes {
-		fmt.Println(n)
+	instructions, err := Compile(program)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ins := range instructions {
+		fmt.Println(ins)
 	}
 }
